13bec8b3cc866d163ab8ab93ef711d4ca608c0d9
Shintaro Sakoda <sakoda@keio.jp>
2023-06-23 17:52:28 +0900

    Added Dockerfile

---
+++ docker/build_image_and_run_container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+
+IMAGE_NAME=$1
+CONTAINER_NAME=$2
+
+cd $(dirname $0)
+
+docker build -t $IMAGE_NAME .
+
+docker run -it \
+--privileged \
+--gpus=all \
+--net=host \
+--env DISPLAY=$DISPLAY \
+--env XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
+--env TERM=xterm-256color \
+--volume /tmp/.X11-unix/:/tmp/.X11-unix \
+--volume $HOME/data:$HOME/data \
+--name $CONTAINER_NAME \
+$IMAGE_NAME bash
