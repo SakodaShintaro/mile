d69166f5774260f3bde2e265feb23612edb19a68
Shintaro Sakoda <sakoda@keio.jp>
2023-07-02 13:18:06 +0900

    Add image_callback
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- ros2/src/mile_pkg/mile_pkg/main.py
+++ ros2/src/mile_pkg/mile_pkg/main.py
@@ -4,6 +4,9 @@ import torch
 from omegaconf import OmegaConf
 from mile.config import get_cfg
 from mile.models.mile import Mile
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
 
 class MileNode(Node):
@@ -18,8 +21,6 @@ class MileNode(Node):
         assert ckpt_path, "ckpt_path is None"
         assert conf_path, "conf_path is None"
 
-        self.timer = self.create_timer(0.1, self.on_tick)
-
         cfg = OmegaConf.load(conf_path)
         cfg = OmegaConf.to_container(cfg)
         cfg = get_cfg(cfg_dict=cfg)
@@ -27,7 +28,8 @@ class MileNode(Node):
 
         model = Mile(cfg)
         checkpoint = torch.load(ckpt_path, map_location='cpu')['state_dict']
-        checkpoint = {key[6:]: value for key, value in checkpoint.items() if key[:5] == 'model'}
+        checkpoint = {key[6:]: value for key,
+                      value in checkpoint.items() if key[:5] == 'model'}
         model.load_state_dict(checkpoint, strict=True)
         model.eval()
         self.get_logger().info(f"loaded weights")
@@ -55,9 +57,10 @@ class MileNode(Node):
         batch['extrinsics'] = torch.zeros((b, s, 4, 4))
         batch['throttle_brake'] = torch.zeros((b, s, 1))
         batch['steering'] = torch.zeros((b, s, 1))
+        self.H = 224
+        self.W = 224
 
         out = model(batch)
-        print(sorted(list(out.keys())))
         """
         dict_keys(['bev_instance_center_1', 'bev_instance_center_2', 'bev_instance_center_4',
                 'bev_instance_offset_1', 'bev_instance_offset_2', 'bev_instance_offset_4',
@@ -65,8 +68,30 @@ class MileNode(Node):
                 'posterior', 'prior', 'steering', 'throttle_brake'])
         """
 
-    def on_tick(self):
-        self.get_logger().info(f"Mile node")
+        self.batch = {}
+
+        # get from ros2 topic
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10
+        )
+        self.bridge = CvBridge()
+        self.sub_image = self.create_subscription(
+            Image, "/sensing/camera/traffic_light/image_raw", self.image_callback, qos_profile)
+        self.get_logger().info(f"Ready")
+
+    def image_callback(self, msg: Image):
+        self.get_logger().info(f"Subscribe Image")
+        cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        ts_image = torch.tensor(cv_image)
+        ts_image = ts_image.permute([2, 0, 1])
+        ts_image = ts_image.unsqueeze(0)
+        ts_image = torch.nn.functional.interpolate(
+            ts_image, size=(self.H, self.W), mode="bilinear")
+        ts_image = ts_image.unsqueeze(0)
+        self.batch['image'] = ts_image
+        self.get_logger().info(f"Image Shape = {ts_image.shape}")
 
 
 def main(args=None):
